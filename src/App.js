bb07735239542e03a79ad3c4e675bfc593648294
Nazar Montsibovych <nazar_montsibovych@epam.com>
2023-03-17 11:23:50 +0200

    HW-1 update GenreList component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import './App.css';
-import { useState } from 'react';
 import Counter from "./components/Counter"
 import SearchInput from './components/SearchInput';
 import GenreList from './components/GenreList';
+import genreList from "./mock/genreData.json"
 
 function App() {
-  const [genre, updateGenre] = useState([
-    { id: 'all', name: 'All', is_active: false },
-    { id: 'documentary', name: 'Documentary', is_active: false },
-    { id: 'comedy', name: 'Comedy', is_active: false },
-    { id: 'horror', name: 'Horror', is_active: false },
-    { id: 'crime', name: 'Crime', is_active: false }
-  ]);
-
-  const updateSelectedItem = (id) => {
-    const updatedItems = genre.map((item) => {
-        if (item.id === id) {
-          return { ...item, is_active: true };
-        }
-        return { ...item, is_active: false };
-      });
-      updateGenre(updatedItems)
-}
   return (
     <div className="App">
      <Counter/>
      <SearchInput/>
      <GenreList 
-        genreList={genre}
+        genreList={genreList}
         currentItem={'horror'}
-        updateSelectedItem={updateSelectedItem}
       />
     </div>
   );
--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -1,19 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./GenreList.scss"
 
 const GenreList = (props) => {
 
-    const { genreList, currentItem, updateSelectedItem } = props;
+    const { genreList, currentItem } = props;
+
+
 
     useEffect(() => {
-        updateSelectedItem(currentItem)
+      updateSelectedItem(currentItem)
     }, [])
 
+    const [items, updateItems] = useState([])
+
+
+
+  const updateSelectedItem = (id) => {
+      const updatedItems = genreList.map((item) => {
+          if (item.id === id) {
+            return { ...item, is_active: true };
+          }
+          return { ...item, is_active: false };
+        });
+        updateItems(updatedItems)
+  }
     
     
     return (
         <ul className="list-container">
-          {genreList.map((item) => (
+          {items.map((item) => (
         <li
           key={item.id}
           className={item.is_active ? 'item' : ''}
